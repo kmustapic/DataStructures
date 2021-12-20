50910b2f0a0bc2442ddbd1d5610acc99141f0865
kmustapic <kristina.mustapic10@gmail.com>
2021-12-20 16:38:46 +0100

    Finished 8th Task

---
+++ Task8/header.h
@@ -0,0 +1,31 @@
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
+
+#include <stdio.h>
+
+struct_node;
+typedef struct _node *NodeP;
+typedef struct _node
+{
+    int data;
+    NodeP left;
+    NodeP right;
+} Node;
+
+void ExecuteTask8();
+void PrintMenu();
+NodeP CreateBST(NodeP root);
+NodeP InsertNode(NodeP currentNodeP, int data);
+void InsertTreeElement(NodeP root);
+NodeP FindNode(NodeP currentNodeP, int data);
+void FindTreeElement(NodeP root);
+NodeP MinValueNode(NodeP currentDirP);
+NodeP DeleteNode(NodeP currentNodeP, int data);
+void DeleteTreeElement(NodeP root);
+void PrintPreorder(NodeP currentNodeP);
+void PrintInorder(NodeP currentNodeP);
+void PrintPostorder(NodeP currentNodeP);
+void BSTPrintout(NodeP root);
+void ExitApp();
+
+#endif
