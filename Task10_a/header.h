fa56e5cb9a05374b929ed5b1f9221f772f203673
kmustapic <kristina.mustapic10@gmail.com>
2022-01-11 11:44:02 +0100

    Finished 10 a) Task

---
+++ Task10_a/header.h
@@ -0,0 +1,36 @@
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
+
+#include <stdio.h>
+
+#define MAX_SIZE 101
+
+struct _city;
+typedef struct _city* CityP;
+struct _city
+{
+    char name[MAX_SIZE];
+    int population;
+    CityP left;
+    CityP right;
+} City;
+
+struct _country;
+typedef struct _country* CountryP;
+struct _country
+{
+    char name[MAX_SIZE];
+    CityP citiesP;
+    CountryP next;
+} Country;
+
+void AddCountryToList(CountryP currentListEl, char* countryName, char* citiesFileName);
+void AddCitiesToTree(CityP currentTreeNode, char* citiesFileName);
+CityP AddCityToTree(CityP newCity, char* cityName, int population);
+void PrintCountries(CountryP currentCountry);
+void PrintCities(CityP currentCity);
+CountryP FindCountry(CountryP currentCountry, char* countryName);
+int FindCity(CityP currentCity, int minValue);
+void ExecuteTask10_a();
+
+#endif
